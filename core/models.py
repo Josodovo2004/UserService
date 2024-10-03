2bab6100050e4293d5cd6453e9eb5de9aea721b0
JoseAguilar <joseaguilarsalazar2004@gmail.com>
2024-10-03 11:39:16 -0500

    refactor: modified register serializer

--- core/models.py
+++ core/models.py
@@ -4,17 +4,3 @@ from django.db import models
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     ruc = models.CharField(max_length=20, blank=True, null=True)
-    # Add other fields as necessary
-
-# Signal to create or update UserProfile whenever User is created or updated
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
